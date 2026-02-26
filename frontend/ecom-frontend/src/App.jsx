039a32e0606f90f1e106dbfaf8ea9c5bcdf73fe6
harshalmandliya <harshalmandliya3@gmail.com>
2026-02-26 09:56:22 +0530

    Add Navbar component and update App layout

--- frontend/ecom-frontend/src/App.jsx
+++ frontend/ecom-frontend/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Products from './components/products/Products';
+import Navbar from './components/shared/Navbar';
 
 function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/products' element={ <Products />}/>
