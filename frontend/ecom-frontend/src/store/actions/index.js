42f1c29b7896602bdac72614c3dff6a4f6de2289
harshalmandliya <harshalmandliya3@gmail.com>
2026-03-09 19:03:40 +0530

    feat(cart): allow users to remove item from cart

--- frontend/ecom-frontend/src/store/actions/index.js
+++ frontend/ecom-frontend/src/store/actions/index.js
@@ -100,3 +100,9 @@ export const addToCart = (data, qty = 1, toast) =>
         });
         localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
     }
+
+    export const removeFromCart =  (data, toast) => (dispatch, getState) => {
+    dispatch({type: "REMOVE_CART", payload: data });
+    toast.success(`${data.productName} removed from cart`);
+    localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
+}
