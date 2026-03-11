08e8dddc9975dab2d9c154393549d50d1ab16a24
harshalmandliya <harshalmandliya3@gmail.com>
2026-03-11 13:59:40 +0530

    feat: implement user registration flow

--- frontend/ecom-frontend/src/store/actions/index.js
+++ frontend/ecom-frontend/src/store/actions/index.js
@@ -1,4 +1,5 @@
-import api from "../../api/api";        
+import api from "../../api/api";      
+import { registerNewUser } from '../../store/actions';
 
 export const fetchProducts = (queryString)=>async(dispatch)=>{
     try {
@@ -120,6 +121,22 @@ export const authenticateSignInUser
         } catch (error) {
             console.log(error);
             toast.error(error?.response?.data?.message || "Internal Server Error");
+        } finally {
+            setLoader(false);
+        }
+    };
+
+    export const registerNewUser 
+    = (sendData, toast, reset, navigate, setLoader) => async (dispatch) => {
+        try {
+            setLoader(true);
+            const { data } = await api.post("/auth/signup", sendData);
+            reset();
+            toast.success(data?.message || "User Registered Successfully");
+            navigate("/login");
+        } catch (error) {
+            console.log(error);
+            toast.error(error?.response?.data?.message || error?.response?.data?.password || "Internal Server Error");
         } finally {
             setLoader(false);
         }
