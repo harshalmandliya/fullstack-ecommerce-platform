d83a13c6ec7d621284a0177619bc320aea959a4a
harshalmandliya <harshalmandliya3@gmail.com>
2026-03-20 01:38:19 +0530

    feat(ui): enhance price formatting for better readability

--- frontend/ecom-frontend/src/utils/formatPrice.js
+++ frontend/ecom-frontend/src/utils/formatPrice.js
@@ -4,3 +4,7 @@ export const formatPrice = (price) => {
     currency: "INR",
   }).format(price);
 };
+
+export const formatPriceCalculation = (quantity, price) => {
+   return (Number(quantity) * Number(price)).toFixed(2);
+  }
