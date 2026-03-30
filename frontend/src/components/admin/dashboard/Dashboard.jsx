ef2754fe4e9b1154d0fe7ecc1beeaba7fab55054
harshalmandliya <harshalmandliya3@gmail.com>
2026-03-30 22:50:47 +0530

    feat: fetch dashboard analytics data from API

--- frontend/src/components/admin/dashboard/Dashboard.jsx
+++ frontend/src/components/admin/dashboard/Dashboard.jsx
@@ -1,9 +1,30 @@
 import DashboardOverview from './DashboardOverview'
 import { FaBoxOpen, FaDollarSign, FaShoppingCart } from 'react-icons/fa';
 import { MdAttachMoney } from 'react-icons/md';
+import { useDispatch, useSelector } from 'react-redux';
+import { analyticsAction } from '../../../store/actions';
+import Loader from '../../shared/Loader';
+import ErrorPage from '../../shared/ErrorPage';
+import { useEffect } from 'react';
 
 const Dashboard = () => {
-      const { productCount, totalRevenue, totalOrders } = { "productCount": "13", "totalRevenue": "8680.0", "totalOrders": "11" };
+ const dispatch = useDispatch();
+  const {isLoading, errorMessage} = useSelector((state) => state.errors);
+  const { 
+    analytics: { productCount, totalRevenue, totalOrders },
+   } = useSelector((state) => state.admin);
+
+   useEffect(() => {
+    dispatch(analyticsAction());
+   }, [dispatch]);
+
+   if (isLoading) {
+    return <Loader />
+   }
+
+   if (errorMessage) {
+    return <ErrorPage message={errorMessage}/>;
+   }
   return (
 <div>
       <div className='flex md:flex-row mt-8 flex-col lg:justify-between 
