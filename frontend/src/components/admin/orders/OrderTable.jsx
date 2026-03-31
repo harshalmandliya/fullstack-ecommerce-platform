5f71806e83ca47727a75b12beefca56490672df5
harshalmandliya <harshalmandliya3@gmail.com>
2026-03-31 21:05:34 +0530

    feat: develop update form for order details

--- frontend/src/components/admin/orders/OrderTable.jsx
+++ frontend/src/components/admin/orders/OrderTable.jsx
@@ -3,9 +3,14 @@ import { adminOrderTableColumn } from '../../helper/tableColumn';
 import { useState } from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-
+import Modal from '../../shared/Modal';
+import UpdateOrderForm from './UpdateOrderForm';
 
 const OrderTable = ({adminOrder, pagination}) => {
+
+    const [updateOpenModal, setUpdateOpenModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("");
+  const [loader, setLoader] = useState(false);
 
     const navigate = useNavigate();
  const [currentPage, setCurrentPage] = useState(
@@ -33,6 +38,13 @@ const handlePaginationChange = (paginationModel) => {
    navigate(`${pathname}?${params}`)
 }
 
+
+const handleEdit = (order) => {
+  setSelectedItem(order);
+  setUpdateOpenModal(true);
+}
+
+
   return (
    <div>
       <h1 className='text-slate-800 text-3xl text-center font-bold pb-6 uppercase'>
@@ -43,7 +55,7 @@ const handlePaginationChange = (paginationModel) => {
          <DataGrid
          className='w-full'
             rows={tableRecords}
-            columns={adminOrderTableColumn}
+            columns={adminOrderTableColumn(handleEdit)}
             paginationMode='server'
             rowCount={pagination?.totalElements || 0}
             initialState={{
@@ -66,6 +78,19 @@ const handlePaginationChange = (paginationModel) => {
             }}
           />
       </div>
+      <Modal
+        open={updateOpenModal}
+        setOpen={setUpdateOpenModal}
+        title='Update Order Status'>
+          <UpdateOrderForm
+            setOpen={setUpdateOpenModal}
+            open={updateOpenModal}
+            loader={loader}
+            setLoader={setLoader}
+            selectedId={selectedItem.id}
+            selectedItem={selectedItem}
+            />
+      </Modal>
     </div>
   )
 }
